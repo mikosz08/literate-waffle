63eaf487584fa12e18ea5a79f96613b4e77d0648
mikosz08 <mziolek.08@gmail.com>
2024-03-27 23:17:29 +0100

    Adds models for tag and api response

---
+++ StackAPI/Models/StackTag.cs
@@ -0,0 +1,11 @@
+﻿namespace StackAPI.Models
+{
+    public class StackTag
+    {
+        public required string Name { get; set; }
+        public int Count { get; set; }
+        public bool HasSynonyms { get; set; }
+        public bool IsModeratorOnly { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
